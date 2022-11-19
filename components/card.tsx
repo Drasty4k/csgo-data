0817bcbbe3de70751879e9b98f4bb0410e785ec0
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-11-19 13:02:39 +0200

    implemented a Info icon that shows an tooltip in the Card UI Component

--- components/card.tsx
+++ components/card.tsx
@@ -1,4 +1,5 @@
-import { ArrowDownIcon, XMarkIcon } from "./icons";
+import { ArrowDownIcon, InfoIcon, XMarkIcon } from "./icons";
+import ReactTooltip from "react-tooltip";
 
 type Props = {
   title: string;
@@ -9,8 +10,9 @@ type Props = {
 const Card: React.FC<Props> = ({ title, data, idToScroll }) => {
   return (
     <a href={`#${idToScroll}`}>
-      <div className="group p-10 bg-gray-800 rounded-3xl text-center space-y-3 hover:bg-[#372f1f] transition hover:cursor-pointer">
+      <div className="relative group p-10 bg-gray-800 rounded-3xl text-center space-y-3 hover:bg-[#372f1f] transition hover:cursor-pointer">
         <h3 className="text-2xl text-orange-300 font-bold">{title}</h3>
+        {!idToScroll && <Info />}
         <p className="font-bold">{data}</p>
         {idToScroll ? (
           <ArrowDownIcon
@@ -31,5 +33,34 @@ const Card: React.FC<Props> = ({ title, data, idToScroll }) => {
     </a>
   );
 };
+
+const Info: React.FC = () => (
+  <>
+    <a data-tip data-for="infoTooltip" className="absolute left-16 top-[68px]">
+      <InfoIcon
+        strokeWidth={1.5}
+        width={30}
+        height={30}
+        className="hover:fill-orange-800"
+      />
+    </a>
+    <ReactTooltip
+      id="infoTooltip"
+      effect="solid"
+      place="left"
+      overridePosition={({ left, top }) => ({
+        top: 165,
+        left,
+      })}
+      textColor="#fff"
+      backgroundColor="#38475B"
+    >
+      <span>
+        This is an estimated amount
+        <br /> (it does not include the spent money on the first round)
+      </span>
+    </ReactTooltip>
+  </>
+);
 
 export default Card;
